2c2f1127e33d989c07f58b789bcbc637b4d5004a
Keder <edufs96@gmail.com>
2020-04-17 16:51:35 +0200

    Init

---
+++ Platilla_3Projects (1)/GameLib/PlayerInfo.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <list>
+#include <SFML\Graphics.hpp>
+
+enum tipo { PERSONAJE, ARMA, SALA };
+
+enum comandos {
+	START, LANZADADOS, ELEGIRPISTA, REVELARPISTA, MOVIMIENTO, NUEVAPOSICION, DEDUCCION, ENVIOSUPOSICION,
+	DESMENTIRCS, DESMENTIRSC, DESMENTIDO, DESMENTIDOGENERAL, RESOLVERCS, RESOLVERSC, RESOLUCIONCORRECTA
+};
+
+enum estados {TURNO, WAIT};
+
+enum habitaciones { BILLAR, BIBLIOTECA, COCINA, BAILE, INVERNADERO, COMEDOR, VESTIBULO, SALON, ESTUDIO};
+
+class carta
+{
+public:
+	tipo cartatipo;
+	std::string nombre;
+	int numero;
+
+	carta(tipo t, std::string s, int n)
+	{
+		cartatipo = t;
+		nombre = s;
+		numero = n;
+	}
+private:
+
+};
+
+class PlayerInfo
+{
+
+	
+	sf::Vector2i position;
+	
+	int lives;
+public:
+	std::string name;
+	int id;
+	float x;
+	float y;
+	std::list<carta> mano;
+	PlayerInfo();
+	~PlayerInfo();
+	habitaciones habitacion;
+};
